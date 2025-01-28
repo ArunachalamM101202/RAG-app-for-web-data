349a5100fd30b955a6946030eeb044434a8e2fad
Arunachalam Manikandan <76060459+ArunachalamM101202@users.noreply.github.com>
2025-01-28 17:11:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ from langchain_core.runnables import RunnablePassthrough
 
 import tempfile
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 import chromadb
 
